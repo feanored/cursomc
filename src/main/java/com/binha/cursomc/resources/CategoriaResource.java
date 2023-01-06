2095ad0500ab44062ddca1c5f657aa70492b385c
Eduardo GM <eduardo.floyd@outlook.com>
2023-01-05 22:33:26 -0300

    Testando a classe de domínio Categoria

--- src/main/java/com/binha/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/binha/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.binha.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +10,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.binha.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST is working on GET!";
+	public List<Categoria> listar() {
+		final List<Categoria> cats = new ArrayList<>();
+		cats.add(new Categoria(1, "Informática"));
+		cats.add(new Categoria(2, "Escritório"));
+		
+		return cats;
 	}
 	
 	@PostMapping
